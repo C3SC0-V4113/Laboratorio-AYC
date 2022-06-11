b22ff9631ed5affa0b15463caab02692b00e8ad2
CESCO <cescovalledev@gmail.com>
2022-06-11 17:33:03 -0600

    Añadiendo otros complementarios

---
+++ Guia_1-Introduccion/formatting2.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 11 15:59:09 2022
+
+@author: frank
+"""
+
+"""
+f-Strings
+
+str.format()
+
+
+"""
+
+name="José"
+age=22
+texto="Hola soy {}. Tengo {}"
+texto.format(name,age)
+
+"""
+Si bien, es más legible que usando %, sigue siendo complicado de usar
+con multiples parametros
+"""
---
+++ Guia_1-Introduccion/formatting3.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 11 16:44:37 2022
+
+@author: frank
+"""
+
+"""
+f-Strings
+
+f-Strings
+
+
+"""
