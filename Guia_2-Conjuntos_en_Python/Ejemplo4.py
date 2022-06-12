e302504ba0d913230e7e2f4a827fcae3cc0488c7
CESCO <cescovalledev@gmail.com>
2022-06-12 00:57:29 -0600

    Haciendo ejercicios

---
+++ Guia_2-Conjuntos_en_Python/Ejemplo4.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 12 00:05:12 2022
+
+@author: frank
+"""
+
+#Unión de conjuntos:
+A={1,2,3,4,5}
+B={4,5,6,7,8,9,10}
+print("Unión de los conjuntos A y B: ",A.union(B))
+
+#Intersección de conjuntos:
+print("Intersección de conjuntos: ",A.intersection(B))
+
+#Diferencia de conjuntos:
+print("Diferencia de conjuntos: ",A-B)
---
+++ Guia_2-Conjuntos_en_Python/Ejemplo5Sympy.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 12 00:12:50 2022
+
+@author: frank
+"""
+
+from sympy import FiniteSet
+C=FiniteSet(1,2,3)
+print("Utilizando FiniteSet: ",C)
+
+#Generando el conjunto potencia
+#Solo se puede hacer con FiniteSet
+Potencia=C.powerset()
+print("Conjunto Potencia: ",Potencia)
+
+#Cardinalidad:
+print("La cardinalidad del conjunto potencia es: ",len(C.powerset()))
+
+#Igualdad
+A=FiniteSet("x","w","z","k")
+B=FiniteSet("x","c","t")
+if A==B:
+    print("True")
+else:
+    print("False")
+    
+#Unión de dos conjuntos:
+UnionConjuntos=A.union(B)
+print("Unión de conjuntos con FiniteSet: ",UnionConjuntos)
+
+#Intersección de conjuntos:
+Interseccion=A.intersect(B)
+print("Intersección de conjuntos con FiniteSet: ",Interseccion)
+
+#Diferencia
+Diferencia=A-B
+print("Diferencia de conjuntos con FiniteSet: ",Diferencia)
