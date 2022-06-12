a7a5a0a9d662cc24cceda96aebb1d4a2e99187a8
CESCO <cescovalledev@gmail.com>
2022-06-11 22:24:08 -0600

    Empezando la guia 2

---
+++ Guia_2-Conjuntos_en_Python/Ejemplo1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 11 21:16:48 2022
+
+@author: frank
+"""
+
+A={1,4,7,9,11}
+print(A)
+
+#Creando un conjunto a partir de una lista:
+Lista=["Toyota","Mitsubishi","Kia","Fiat"]
+
+#Mostrando la lista:
+print("La lista creada: ",Lista)
+
+#Ahora, la lista la convertimos en un conjunto:
+Conjunto=set(Lista)
+print("El conjunto creado: ",Conjunto)
